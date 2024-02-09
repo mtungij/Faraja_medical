992e4801fbd8c7e68241f2815fef1de22db4c0b9
james <jamesmtungi2023@gmail.com>
2024-02-08 23:11:18 -0800

    rch index

--- app/Views/rches/index.php
+++ app/Views/rches/index.php
@@ -9,17 +9,7 @@
         <div class="bg-white w-full">
             <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                 <div class="w-full">
-                    <form class="flex items-center">
-                        <label for="simple-search" class="sr-only">Search</label>
-                        <div class="relative w-full">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                            <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" required="">
-                        </div>
-                    </form>
+                    
                 </div>
                 <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
                     <button data-modal-target="rch-modal" data-modal-toggle="rch-modal" type="button" class="flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
@@ -68,27 +58,27 @@
                 </div>
             </div>
             <div class="overflow-x-auto">
-                <table class="w-full border border-gray-200 rounded text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 border-b border-gray-300 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <table id="myTable"  class="display">
+                    <thead>
                         <tr>
-                            <th scope="col" class="px-4 py-3">S/N</th>
-                            <th scope="col" class="px-4 py-3">NAME</th>
-                            <th scope="col" class="px-4 py-3">PRICE</th>
-                            <th scope="col" class="px-4 py-3">
-                                <span class="sr-only">Actions</span>
+                            <th>S/N</th>
+                            <th>NAME</th>
+                            <th>PRICE</th>
+                            <th>
+                                <span>Actions</span>
                             </th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php $rowId = 1 ;?>
                         <?php foreach($rches as $rch): ?>
-                            <tr class="border-b dark:border-gray-700">
-                                <th scope="row" class="px-4 py-3">
+                            <tr>
+                                <th>
                                     <?= $rowId++ ?>
                                 </th>
-                                <td class="px-4 py-3"><?= $rch->name ?></td>
-                                <td class="px-4 py-3"><?= number_format($rch->price) ?></td>
-                                <td class="px-4 py-3 flex items-center gap-1 justify-end">
+                                <td><?= $rch->name ?></td>
+                                <td><?= number_format($rch->price) ?></td>
+                                <td>
                                     <button data-modal-target="edit-rch<?= $rch->id ?>" data-modal-toggle="edit-rch<?= $rch->id ?>" class="font-medium px-3 py-1 rounded border border-gray-300 text-green-600 dark:text-green-500 hover:underline">Edit</a>
                                 </td>
 
